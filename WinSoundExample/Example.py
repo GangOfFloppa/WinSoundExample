beb0ef613cd28d52702422a682f65d0bd460c9d9
Quaxdrone(Nijo Fukushi) <137780492+Quaxdrone@users.noreply.github.com>
2023-07-29 17:33:22 +0300

    Add files via upload

---
+++ WinSoundExample/Example.py
@@ -0,0 +1,16 @@
+import winsound
+import os
+import sys
+
+def GetSoundName():
+    return "SudnoBoris.wav"
+def PlaySound():
+    flags = winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP
+    winsound.PlaySound(GetSoundName(), flags)
+if __name__ == "__main__":
+    PlaySound()
+    zzz = input("Please Write 'Exit' to Close This Python Script: ")
+    if zzz == "Exit":
+        exit(445)
+    else:
+        print("FAILED!!!")
